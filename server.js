a1979600ad2c63be5fe0f736d9c7b89e14cb3a56
Thariqalfa <zrav420@gmail.com>
2019-09-23 15:10:29 +0700

    update cors

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3003;
+const cors = require('cors');
+
+// allow request
+app.use(cors());
 
 /* ROUTER */
 const ArticlesRouter = require('./routers/ArticlesRouter');
@@ -11,7 +15,7 @@ app.use(express.json());
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello World'));
 app.use('/articles', ArticlesRouter);
 
-app.listen(PORT, () => console.log(`Example app listening on port ` + PORT));
+app.listen(PORT, () => console.log('Example app listening on port ' + PORT));
